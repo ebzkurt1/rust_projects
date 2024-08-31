ef1b52338d5a9f0e621422693f3ab3bea83d052a
ebzkurt1 <emrebzkurt1@gmail.com>
2024-08-31 21:48:20 +0300

    ADD: hashmap to record number of occurance

--- huffman_encoder/src/main.rs
+++ huffman_encoder/src/main.rs
@@ -1,7 +1,18 @@
 mod txt_reader;
 
 use std::io::{self, Write};
+use std::collections::HashMap;
 
+
+fn count_characters(input: &str) -> HashMap<char, usize> {
+    let mut character_counts = HashMap::new();
+
+    for character in input.chars() {
+        *character_counts.entry(character).or_insert(0) += 1;
+    }
+
+    character_counts
+}
 
 fn main() {
     print!("Please enter the filename: ");
@@ -13,7 +24,13 @@ fn main() {
     let file_name = file_name.trim();
 
     match txt_reader::read_file_contents(file_name) {
-        Ok(contents) => println!("File contents:\n{}", contents),
+        Ok(contents) => {
+            let character_counts = count_characters(&contents);
+            print!("Character counts:");
+            for (char, count) in &character_counts {
+                println!("'{}' occurs {} time(s)", char, count);
+            }
+        }
         Err(e) => eprintln!("Failed to read file: {}", e),
     }
 }
